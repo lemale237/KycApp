ebbcee344f5d1944c6bf98ea0fe0771f374587e1
lemale <kamtsou1997@gmail.com>
2021-08-17 23:27:36 +0100

    commit

---
+++ app/src/main/java/com/example/kycapp/AgentOneInfosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kycapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgentOneInfosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agent_one_infos)
+    }
+}
---
+++ app/src/main/java/com/example/kycapp/ProgressBarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kycapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProgressBarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_progress_bar)
+    }
+}
