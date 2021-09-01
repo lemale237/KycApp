b3bf77369b80fe711d6f56d0068246a4f80fcf88
lemale <kamtsou1997@gmail.com>
2021-09-01 16:37:34 +0100

    commit

--- app/src/main/java/com/example/kycapp/LoginActivity.kt
+++ app/src/main/java/com/example/kycapp/LoginActivity.kt
@@ -1,11 +1,39 @@
-package com.example.kycapp
+package com.example.kycapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.example.kycapp.MainActivity
+import com.example.kycapp.R
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private var btn_connexion: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btn_connexion = findViewById<View>(R.id.btn_connexion) as ImageView
+        btn_connexion!!.setOnClickListener {
+
+            var progressBar = findViewById<View>(R.id.progress_Bar) as ProgressBar
+            progressBar.isVisible = true
+
+            Handler().postDelayed(
+                {
+                    progressBar.isVisible = false
+                    val otherActivity = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(otherActivity)
+                    finish()
+                }, 3000)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/kycapp/SplashScreenAcivity.kt
+++ app/src/main/java/com/example/kycapp/SplashScreenAcivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.kycapp.ui.LoginActivity
 
 class SplashScreenAcivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen_acivity)
         Handler().postDelayed({
-            var intent = Intent(this, MainActivity::class.java)
+            var intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }, 5000)
     }
