b4574a01937c7702b6561f0574fbcc275112d242
lemale <kamtsou1997@gmail.com>
2021-09-03 15:33:09 +0100

    Mise à jour des détails

--- app/src/main/java/com/example/kycapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/kycapp/ui/dashboard/DashboardFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -36,6 +37,11 @@ class DashboardFragment : Fragment() {
             textView.text = it
         })
         return root
+
+
+
+
+
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/kycapp/ui/successRegistration/SucessRegistrationFragment.kt
+++ app/src/main/java/com/example/kycapp/ui/successRegistration/SucessRegistrationFragment.kt
@@ -1,5 +1,6 @@
 package com.example.kycapp.ui.successRegistration
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +8,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kycapp.R
+import android.os.Handler
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.kycapp.ui.LoginActivity
+import com.example.kycapp.ui.dashboard.DashboardFragment
+
 
 class SucessRegistrationFragment : Fragment() {
+
 
     companion object {
         fun newInstance() = SucessRegistrationFragment()
@@ -21,12 +29,21 @@ class SucessRegistrationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.sucess_registration_fragment, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SucessRegistrationViewModel::class.java)
         // TODO: Use the ViewModel
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed({
+         findNavController().navigate(R.id.action_sucessRegistrationFragment_to_navigation_dashboard)
+        }, 3000)
+
+
     }
+
+
 
 }
